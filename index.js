c39a56519e828047a3e0f5da2ac6820f29a1328a
mahadiduet <mahadiduet@gmail.com>
2024-08-21 15:19:01 +0600

    search option api done

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());;
 app.use(cors({
-    origin: [
-        'http://localhost:5173'
-    ],
-    credentials: true
+  origin: [
+    'http://localhost:5173'
+  ],
+  credentials: true
 }));
 
 app.get('/', (req, res) => {
-    res.send('Server side running...')
+  res.send('Server side running...')
 })
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -37,13 +37,53 @@ async function run() {
 
     // Add Product
     app.post('/add-product', async (req, res) => {
-        const product = req.body;
-        // console.log('11');
-        console.log(product);
-        const result = await productCollection.insertOne(product);
-        console.log(`A document was inserted with the _id: ${result}`);
-        res.send(result);
-     })
+      const product = req.body;
+      // console.log('11');
+      console.log(product);
+      const result = await productCollection.insertOne(product);
+      console.log(`A document was inserted with the _id: ${result}`);
+      res.send(result);
+    })
+
+
+    // All Product count
+    app.get('/allProductCount', async (req, res) => {
+      const dataCount = await productCollection.countDocuments();
+      res.send(String(dataCount));
+    })
+
+    // All tourism spot API
+    app.get('/all-product', async (req, res) => {
+      const data = await productCollection.find().sort({ _id: -1 }).toArray();
+      res.send(data);
+    })
+
+    // Product Search
+    app.get('/search', async (req, res) => {
+      const searchValue = req.query.query;
+      console.log(searchValue);
+      try {
+        const searchQuery = { productName: { $regex: searchValue, $options: 'i' } };
+        const data = await productCollection.find(searchQuery).sort({ _id: -1 }).toArray();
+        res.send(data);
+      } catch (error) {
+        res.status(500).json({ message: err.message });
+      }
+
+    })
+    // app.get('/search', async (req, res) => {
+    //   console.log('sss');
+    //   const searchQuery = req.query.query || '';
+    //   console.log('ggg',searchQuery);
+    //   try {
+    //     const results = await productCollection.find({
+    //       productName: { $regex: searchQuery, $options: 'i' }
+    //     });
+    //     res.json(results);
+    //   } catch (err) {
+    //     res.status(500).json({ message: err.message });
+    //   }
+    // });
 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -57,5 +97,5 @@ run().catch(console.dir);
 
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`)
 })
