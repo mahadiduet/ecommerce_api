a1f5efe4b5b77d3a6c8cd023a8aff55f8a32567d
mahadiduet <mahadiduet@gmail.com>
2024-08-24 23:19:43 +0600

    brand and category api done

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ app.use(express.json());
 app.use(cookieParser());;
 app.use(cors({
   origin: [
-    'http://localhost:5173'
+    'http://localhost:5173',
+    // 'https://ecommerce-743b3.web.app'
   ],
   credentials: true
 }));
@@ -33,35 +34,57 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // await client.connect();
+    // ****************Start Collection **********************
     const productCollection = client.db("ecommerceDB").collection("product");
+    const brandCollection = client.db("ecommerceDB").collection("brand");
+    const categoryCollection = client.db("ecommerceDB").collection("category");
+    // ****************End Collection**********************
 
+
+    // ****************Start Product API**********************
     // Add Product
     app.post('/add-product', async (req, res) => {
       const product = req.body;
-      // console.log('11');
-      console.log(product);
       const result = await productCollection.insertOne(product);
-      console.log(`A document was inserted with the _id: ${result}`);
       res.send(result);
     })
 
 
-    // All Product count
+    // Product count
     app.get('/allProductCount', async (req, res) => {
       const dataCount = await productCollection.countDocuments();
       res.send(String(dataCount));
     })
 
-    // All tourism spot API
+    // All Product get
     app.get('/all-product', async (req, res) => {
       const data = await productCollection.find().sort({ _id: -1 }).toArray();
       res.send(data);
     })
 
+    // Specific product get for edit product data
+    app.get('/edit-product/:id', async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const item = await productCollection.findOne({ _id: new ObjectId(id) });
+      res.send(item);
+    })
+
+    // Product Update 
+    app.patch('/edit-product/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updateData = {
+        $set: req.body
+      };
+      const result = await productCollection.updateOne(filter, updateData, options);
+      res.send(result);
+    })
+
     // Product Search
     app.get('/search', async (req, res) => {
       const searchValue = req.query.query;
-      console.log(searchValue);
       try {
         const searchQuery = { productName: { $regex: searchValue, $options: 'i' } };
         const data = await productCollection.find(searchQuery).sort({ _id: -1 }).toArray();
@@ -71,25 +94,42 @@ async function run() {
       }
 
     })
-    // app.get('/search', async (req, res) => {
-    //   console.log('sss');
-    //   const searchQuery = req.query.query || '';
-    //   console.log('ggg',searchQuery);
-    //   try {
-    //     const results = await productCollection.find({
-    //       productName: { $regex: searchQuery, $options: 'i' }
-    //     });
-    //     res.json(results);
-    //   } catch (err) {
-    //     res.status(500).json({ message: err.message });
-    //   }
-    // });
+    // ****************End Product**********************
 
-    await client.db("admin").command({ ping: 1 });
+    // ****************Start Brand**********************
+    // Add Brand
+    app.post('/add-brand', async (req, res) => {
+      const brand = req.body;
+      const result = await brandCollection.insertOne(brand);
+      res.send(result);
+    })
+
+    // All Brand 
+    app.get('/brand', async (req, res) => {
+      const data = await brandCollection.find().sort({ _id: -1 }).toArray();
+      res.send(data);
+    })
+    // ****************End Brand************************
+    // ****************Start Category************************
+    // Add Category
+    app.post('/add-category', async (req, res) => {
+      const brand = req.body;
+      const result = await categoryCollection.insertOne(brand);
+      res.send(result);
+    })
+
+    // All Category 
+    app.get('/category', async (req, res) => {
+      const data = await categoryCollection.find().toArray();
+      res.send(data);
+    })
+    // ****************End Category************************
+
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } catch (err) {
+    console.error('Error connecting to MongoDB:', err);
   } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
+    // await client.close(); 
   }
 }
 run().catch(console.dir);
